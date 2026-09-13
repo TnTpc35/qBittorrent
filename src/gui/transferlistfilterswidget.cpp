caa4a60f16d03dc1907dbec9d24aefb7fc71acc9
TnT_PC <tntbilgisayar35@gmail.com>
2026-09-13 17:31:45 +0300

    Widen and space the modern filters sidebar

--- src/gui/transferlistfilterswidget.cpp
+++ src/gui/transferlistfilterswidget.cpp
@@ -1,32 +1,3 @@
-/*
- * Bittorrent Client using Qt and libtorrent.
- * Copyright (C) 2023-2025  Vladimir Golovnev <glassez@yandex.ru>
- * Copyright (C) 2006  Christophe Dumez <chris@qbittorrent.org>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * In addition, as a special exception, the copyright holders give permission to
- * link this program with the OpenSSL project's "OpenSSL" library (or with
- * modified versions of it that use the same license as the "OpenSSL" library),
- * and distribute the linked executables. You must obey the GNU General Public
- * License in all respects for all of the code used other than "OpenSSL".  If you
- * modify file(s), you may extend this exception to your version of the file(s),
- * but you are not obligated to do so. If you do not wish to do so, delete this
- * exception statement from your version.
- */
-
 #include "transferlistfilterswidget.h"
 
 #include <QIcon>
@@ -54,14 +25,18 @@ TransferListFiltersWidget::TransferListFiltersWidget(QWidget *parent, TransferLi
     , m_transferList {transferList}
 {
     setBackgroundRole(QPalette::Base);
+    setMinimumWidth(245);
+    setObjectName(u"modernFiltersSidebar"_s);
+    setAttribute(Qt::WA_StyledBackground, true);
 
     Preferences *const pref = Preferences::instance();
 
-    // Construct lists
     auto *mainWidget = new QWidget;
+    mainWidget->setObjectName(u"modernFiltersSidebarContent"_s);
+    mainWidget->setAttribute(Qt::WA_StyledBackground, true);
     auto *mainWidgetLayout = new QVBoxLayout(mainWidget);
-    mainWidgetLayout->setContentsMargins(0, 2, 0, 0);
-    mainWidgetLayout->setSpacing(2);
+    mainWidgetLayout->setContentsMargins(8, 10, 8, 10);
+    mainWidgetLayout->setSpacing(10);
     mainWidgetLayout->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 
     {
